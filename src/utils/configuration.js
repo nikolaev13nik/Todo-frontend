fbe72eaed1994456e18b44064166d8e9b73a9082
Nikita Nikolaev <Nikolaevnik13@gmail.com>
2024-09-19 22:57:19 +0300

    urls

--- src/utils/configuration.js
+++ src/utils/configuration.js
@@ -1,10 +1,10 @@
 
-export const URI_LOGIN="http://localhost:8080/account/login"
-export const URI_NEW_USER="http://localhost:8080/account/user"
-export const URI_DELETE_TASK="http://localhost:8080/task/"
-export const URI_EDIT_TASK="http://localhost:8080/task/update/"
-export const URI_ADD_TASK="http://localhost:8080/task/"
-export const URI_REMOVE_ALL_TASKS="http://localhost:8080/task/clear/tasks/"
+// export const URI_LOGIN="http://localhost:8080/account/login"
+// export const URI_NEW_USER="http://localhost:8080/account/user"
+// export const URI_DELETE_TASK="http://localhost:8080/task/"
+// export const URI_EDIT_TASK="http://localhost:8080/task/update/"
+// export const URI_ADD_TASK="http://localhost:8080/task/"
+// export const URI_REMOVE_ALL_TASKS="http://localhost:8080/task/clear/tasks/"
 
 
 // export const URI_LOGIN="https://app-todo-tasks-backend.herokuapp.com/account/login"
@@ -14,4 +14,10 @@ export const URI_REMOVE_ALL_TASKS="http://localhost:8080/task/clear/tasks/"
 // export const URI_ADD_TASK="https://app-todo-tasks-backend.herokuapp.com/task/"
 // export const URI_REMOVE_ALL_TASKS="https://app-todo-tasks-backend.herokuapp.com/task/clear/tasks/"
 
+export const URI_LOGIN="https://todo-backend-app-48be291ae2f8.herokuapp.com/account/login"
+export const URI_NEW_USER="https://todo-backend-app-48be291ae2f8.herokuapp.com/account/user"
+export const URI_DELETE_TASK="https://todo-backend-app-48be291ae2f8.herokuapp.com/task/"
+export const URI_EDIT_TASK="https://todo-backend-app-48be291ae2f8.herokuapp.com/task/update/"
+export const URI_ADD_TASK="https://todo-backend-app-48be291ae2f8.herokuapp.com/task/"
+export const URI_REMOVE_ALL_TASKS="hhttps://todo-backend-app-48be291ae2f8.herokuapp.com/task/clear/tasks/"
 
